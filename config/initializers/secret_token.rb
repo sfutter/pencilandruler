390e82bc886fdc91b70eaac5da39e4a3903bf2c0
Steven Futter <futter.steven@gmail.com>
2014-03-23 13:04:25 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pencilandruler::Application.config.secret_key_base = 'f729ad9fb148e3a7424229836811b227d7f73bff35f4d24b5372d9ebd5e49025fa946b080c632ca3381abf3d1d104b35e813a2f9210a2a18dedb43b7ed068bae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pencilandruler::Application.config.session_store :cookie_store, key: '_pencilandruler_session'
